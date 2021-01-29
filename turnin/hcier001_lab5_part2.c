e09c6134a08b3d940f42851b995ac59ecc494cd8
shrimpnoodles <77302524+shrimpnoodles@users.noreply.github.com>
2021-01-28 20:18:02 -0800

    Update hcier001_lab5_part2.c

--- turnin/hcier001_lab5_part2.c
+++ turnin/hcier001_lab5_part2.c
@@ -11,155 +11,104 @@
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
-enum States {start, init, plus, minus, wait1, wait2, reset} state;
-unsigned char count;
-//unsigned char button=~PINA;
+enum States {start, init, p0push, p1push, wait1, wait2, reset} state;
+unsigned portcCount;
 void Tick(){
-	switch (state) { //transitions
+	switch(state) { //transitions
 		case start:
 			state = init;
 			break;
 		case init:
 			if((~PINA&0x03) == 0x01){
-				state = plus;
+				state = p0push;
 			}
-			else if((~PINA&0x03) == 0x02){
-				state = minus;
+			else if((~PINA&0x03)== 0x02){
+				state = p1push;
+			}
+			else if((~PINA&0x03)== 0x03){
+				state = reset;
+			}
+			break;
+		case p0push:
+			state = wait1;
+			break;
+		case wait1:
+			if((~PINA&0x03) == 0x02){
+				state = p1push;
 			}
 			else if((~PINA&0x03) == 0x03){
 				state = reset;
 			}
 			else{
-				state = init;
-			}
-			break;
-		case plus:
-			//state = wait1;
-			if((~PINA&0x03)==0x02){
-				//state = minus;
-				state = init;
-			}
-			else if((~PINA&0x03)== 0x03){
-				state = reset;
-			}
-			else if((~PINA&0x03)==0x00){
-				state = init;
-			}
-			else{
-				state = plus;
-			}
-			break;
-		case minus:
-			//state = wait2;
-			if((~PINA&0x03) == 0x03){
-				state = reset;
-			}
-			else if((~PINA&0x03) == 0X00){
-				state = init;
-			}
-			else if((~PINA&0x03) == 0X01){
-				//state = plus;
-				state = init;
-			}
-			else {
-				state = minus;
-			}
-			break;
-		/*case wait1:
-			if(~PINA!=0x01){
-				state = init;
-			}
-			else{
 				state = wait1;
 			}
 			break;
+		case p1push:
+			state = wait2;
+			break;
 		case wait2:
-			if(~PINA!=0x02){
-				state = init;
+			if((~PINA&0x03)==0x03){
+				state = reset;
+			}
+			else if((~PINA&0x03)==0x01){
+				state = p0push;
 			}
 			else{
 				state = wait2;
 			}
-			break;*/
+			break;
 		case reset:
-			if((~PINA&0x03) ==0x03){
+			if((~PINA&0x03) == 0x03){
 				state = reset;
 			}
-			else{
-				state = init;
+			else if((~PINA&0x03)==0x01){
+				state = p0push;
+			}
+			else if((~PINA&0x03)==0x02){
+				state = p1push;
 			}
 			break;
 		default:
 			state = start;
 			break;
-	}
-	switch (state) { //actions
+		}
+	switch(state){ //actions
 		case init:
-			if((~PINA&0x03)==0x01){
-				if(count <9){
-				count++;
-				}
-			}
-			else if((~PINA&0x03) ==0x02){
-				if(count >0){
-				count --;
-				}
-			}
-			else if((~PINA&0x03)==0x03){
-				count = 0;
+			portcCount = 7;
+			break;
+		case p0push:
+			if(portcCount <= 9){
+				portcCount = portcCount +1;
 			}
 			break;
-		case plus:
-			//if(count <9){
-			//	count = count +1;
-			//}
-			if((~PINA&0x03)== 0x02){
-				if(count >0){
-					count--;
-				}
-			}
-			else if((~PINA&0x03) ==0x03){
-				count =0;
+		case p1push: 
+			if(portcCount > 0){
+				portcCount = portcCount -1;
 			}
 			break;
-		case minus:
-			//if(count >0){
-			//	count= count -1;
-			//}
-			if((~PINA&0x03) ==0x01){
-				if(count <9){
-				count++;
-				}
-			}
-			else if((~PINA&0x03) ==0x03){
-				count =0;
-			}
-			break;
-		/*case wait1:
+		case wait1:
 			break;
 		case wait2:
-			break;*/
+			break;
 		case reset:
-			count = 0;
+			portcCount =0;
 			break;
 		default:
 			break;
 	}
 }
-
 int main(void) {
     /* Insert DDR and PORT initializations */
-	DDRA =0X00; PORTA = 0XFF;
-	DDRB  =0Xff; PORTB = 0X00;
-	DDRC = 0XFF; PORTC = 0X00;
+	DDRA = 0X00; PORTA = 0XFF;
+	DDRB = 0XFF; PORTB = 0X00;
+	DDRC = 0xFF; PORTC = 0X00;
 	state = start;
-//	button =~PINA;
-//	button = (button&0x07);
-	count =7;
+	portcCount = 7;
     /* Insert your solution below */
     while (1) {
 	Tick();
-	PORTC = count;
+	PORTC = portcCount;
     }
+
     return 1;
 }
